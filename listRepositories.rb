be3f7a8561fb1e231b46b9c13d4da2411899dc51
Henri Tremblay <henri.tremblay@gmail.com>
2016-02-15 23:57:22 -0500

    List all repositories of a user

---
+++ listRepositories.rb
@@ -0,0 +1,17 @@
+#!/usr/bin/env ruby
+
+require 'json'
+require 'open-uri'
+
+fail 'Usage: listRepositories.rb user_or_organization' unless ARGV.length == 1
+
+user=ARGV[0]
+uri="https://api.github.com/users/#{user}/repos"
+
+file = open(uri)
+res = file.read
+res = JSON.parse(res)
+
+res.map do |s|
+  puts s['name']
+end
